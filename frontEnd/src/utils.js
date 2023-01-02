e60eddb40af30e425ff3f80d826b8b6238063066
andressomoza <andressomozasierra@gmail.com>
2023-01-02 16:48:14 +0100

    loading message

--- frontEnd/src/utils.js
+++ frontEnd/src/utils.js
@@ -7,3 +7,27 @@ export const parseRequestUrl = () => {
         action: request[3],
     }
 }
+
+export const showLoading = () => {
+    document.getElementById('loading-overlay').classList.add('active');
+};
+
+export const hideLoading = () => {
+    document.getElementById('loading-overlay').classList.remove('active');
+};
+
+export const showMessage = (message, callback) => {
+    document.getElementById('message-overlay').innerHTML = `
+    <div>
+        <div id="message-overlay-content">${message}</div>
+        <button id="message-overlay-close-button">OK</button>
+    </div>
+    `;
+    document.getElementById('message-overlay').classList.add('active');
+    document.getElementById('message-overlay-close-button').addEventListener('click', () => {
+        document.getElementById('message-overlay').classList.remove('active');
+        if (callback) {
+            callback();
+        }
+    });
+};
