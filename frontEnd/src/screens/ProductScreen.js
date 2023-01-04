c9b63c1581a1589fb853a9de68271c3bd67ff790
AlessandroBinci <S1098213@studenti.univpm.it>
2023-01-04 17:35:29 +0100

    Build Product API

--- frontEnd/src/screens/ProductScreen.js
+++ frontEnd/src/screens/ProductScreen.js
@@ -1,6 +1,13 @@
+import { getProduct } from '../api';
+
 const ProductScreen = {
-    render:() => {
-        return `<div>ProductScreen</div>`;
+    render: async () => {
+        const request = parseRequestUrl();
+        const product = await getProduct(request.id);
+        if(product.error) {
+            retrun`<div>${product.error}</div>`;
+        }
+        return `<h1>${product.name}</h1>`;
     },
 };
 export default ProductScreen;
