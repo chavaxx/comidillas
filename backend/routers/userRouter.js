50ef44e1ed1edbf673b829e0452c69708fd12c2b
andressomoza <andressomozasierra@gmail.com>
2023-01-10 18:27:41 +0100

    RegisterScreen and ProfileScreen

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import User from '../models/userModel';
 import expressAsyncHandler from 'express-async-handler';
-import { generateToken } from '../utils';
+import { generateToken, isAuth } from '../utils';
 
 
 const userRouter = express.Router();
@@ -39,6 +39,53 @@ userRouter.post('/signin', expressAsyncHandler(async (req,res) =>{
             email: signinUser.email,
             isAdminL: signinUser.isAdmin,
             token: generateToken(signinUser),
+        });
+    }
+}));
+
+userRouter.post('/register', expressAsyncHandler(async (req,res) =>{
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
+    const createdUser = await user.save();
+    if(!createdUser){
+        res.status(401).send({
+            message:'Invalid user data',
+        });
+
+    }
+    else{
+        res.send({
+            _id: createdUser._id,
+            name: createdUser.name,
+            email: createdUser.email,
+            isAdminL: createdUser.isAdmin,
+            token: generateToken(createdUser),
+        });
+    }
+}));
+
+userRouter.put('/:id', isAuth, expressAsyncHandler(async (req,res) =>{
+    const user = await User.findById(req.params.id);
+    if(!user){
+        res.status(404).send({
+            message:'User not found',
+        });
+
+    }
+    else{
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        user.password = req.body.password || user.password;
+        const updatedUser = await user.save();
+        res.send({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdminL: updatedUser.isAdmin,
+            token: generateToken(updatedUser),
         });
     }
 }));
--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -5,16 +5,16 @@ import Error404Screen from "./screens/Error404Screen.js";
 import CartScreen from "./screens/CartScreen.js";
 import SigninScreen from "./screens/SigninScreen.js";
 import Header from "./components/Header.js";
-//import ProfileScreen from "./screens/ProfileScreen.js";
-//import RegisterScreen from "./screens/RegisterScreen.js";
+import ProfileScreen from "./screens/ProfileScreen.js";
+import RegisterScreen from "./screens/RegisterScreen.js";
 const routes = {
     '/': HomeScreen,
     '/product/:id': ProductScreen,
     '/cart/:id':CartScreen,
     '/cart': CartScreen,
     '/signin': SigninScreen,
-    //'/register': RegisterScreen,
-    //'/profile': ProfileScreen,
+    '/register': RegisterScreen,
+    '/profile': ProfileScreen,
 }
 const router = async () => {
     showLoading();
@@ -29,7 +29,7 @@ const router = async () => {
     await Header.after_render();
     const main = document.getElementById('main-container');
     main.innerHTML = await screen.render();
-    await screen.after_render();
+    if (screen.after_render) await screen.after_render();
     hideLoading();
 };
 window.addEventListener('load', router);
