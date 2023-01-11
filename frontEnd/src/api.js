2285669812341458bb33c541393498a7ab2bcfd4
andressomoza <andressomozasierra@gmail.com>
2023-01-11 15:26:40 +0100

    Display order history

--- frontEnd/src/api.js
+++ frontEnd/src/api.js
@@ -1,6 +1,7 @@
 import { apiUrl } from "./config";
 import axios from 'axios';
 import { getUserInfo } from "./localStorage";
+import { response } from "express";
 
 export const getProduct = async (id) => {
     try {
@@ -117,3 +118,43 @@ export const createOrder = async(order) => {
         return {error: (err.response? err.response.data.message: err.message)};
     }
 };
+
+export const getOrder = async(id) => {
+    try {
+        const {token} = getUserInfo();
+        const data = await axios({
+            url: `${apiUrl}/api/orders/${id}`,
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (response.statusText !== 'OK') {
+            throw new Error(response.data.message);
+        }
+        return response.data;   
+    } catch (err) {
+        return { error: err.message };
+    }
+    
+};
+
+export const getMyOrders = async() => {
+    try {
+        const {token} = getUserInfo();
+        const response = await axios({
+            url: `${apiUrl}/api/orders/mine`,
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (response.statusText !== 'OK') {
+            throw new Error(response.data.message);
+        }
+        return response.data;
+    } catch (err) {
+        return { error: err.response ? err.response.data.message : err.message };
+    }
+    
+}
